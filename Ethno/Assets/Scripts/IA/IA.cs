482ff5f3dd7d0f43f751a8a487912dc1e10e483a
Jeremie <jeremie.debarry@gmail.com>
2016-01-30 00:43:28 +0100

    Spawn IA

---
+++ Ethno/Assets/Scripts/IA/IA.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IA : MonoBehaviour {
+
+	public EEthni 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Ethno/Assets/Scripts/IA/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    [SerializeField]
+    private int NombreIA = 0;
+
+    public GameObject GO;
+
+    private float SpawnRadius;
+
+	void Start ()
+    {
+        SpawnRadius = transform.localScale.x / 2;
+        GetComponent<MeshRenderer>().enabled = false;
+		Spawn();
+	}
+	
+	void Update ()
+    {
+     
+	}
+
+    private void Spawn()
+    {
+		for (int i = 0; i <= NombreIA; ++i)
+        {
+            Vector3 newPos = Random.insideUnitCircle * SpawnRadius;
+			newPos.z = newPos.y;
+			Debug.Log("X = " + newPos.x + ", Y = " + newPos.y + ", Z = " + newPos.z);
+			GameObject tmp = GameObject.Instantiate(GO, this.transform.position + newPos, GO.transform.rotation) as GameObject;
+			tmp.transform.position = new Vector3(tmp.transform.position.x, 1, tmp.transform.position.z);
+        }
+    }
+}
