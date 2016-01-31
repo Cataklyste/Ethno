799d1c066e094607d28b6e6cc4b004af0736cc01
Julien Falcioni <dragon9000@hotmail.fr>
2016-01-31 05:55:15 +0100

    change roll sprites, add image preview of the collected roll

---
+++ Ethno/Assets/Scripts/SymboleMenu/SignPreview.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SignPreview : MonoBehaviour {
+
+    public float displayTime = 2.0f;
+    private float currentTime;
+    private SpriteRenderer preview;
+	// Use this for initialization
+	void Start ()
+    {
+        preview = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (currentTime > 0.0f)
+        {
+            currentTime -= Time.deltaTime;
+            if (currentTime <= 0.0f)
+                preview.sprite = null;
+        }
+	}
+
+    public void ResetTimer()
+    {
+        currentTime = displayTime;
+    }
+}
